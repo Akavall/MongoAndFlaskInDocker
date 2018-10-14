28b29327b3bf80aecd14d52339808ac39d67dc49
Kirill <Akavall@gmail.com>
2018-10-14 14:42:09 -0700

    not hardcoding local ip in mongo_utils

--- flask_service/service/mongo_utils.py
+++ flask_service/service/mongo_utils.py
@@ -7,8 +7,9 @@ def insert(collection, record):
 
     print "writing record: {} to {}".format(record, collection)
 
-    # myclient = pymongo.MongoClient("mongodb://localhost:27017/")
-    myclient = pymongo.MongoClient("mongodb://192.168.0.4:27017/")
+    # because mongo-service is specified in docker-compose
+    # we can use it to specify the service
+    myclient = pymongo.MongoClient("mongodb://mongo-service:27017/")
 
     mydb = myclient[database]
     mycol = mydb[collection]
